242325ca9abea9c0f21822ed05e15aa91570f5ae
Curtis Holland <curtis.holland@beaconfire.com>
2016-08-30 11:01:47 -0400

    moving var validation

--- Base/BaseDataTypeClass.php
+++ Base/BaseDataTypeClass.php
@@ -6,5 +6,22 @@ use OpenAir\OpenAir;
 
 class BaseDataTypeClass extends OpenAir
 {
+    function __get($name){
+        if(isset($this->data)){
+            if (array_key_exists($name, $this->data)) {
+                return $this->data[$name];
+            }
+        }
+    }
 
+    function __set($name, $value){
+        if(isset($this->data)){
+            if (array_key_exists($name, $this->data)) {
+                $this->data[$name] = $value;
+                return true;
+            }else{
+                return false;
+            }
+        }
+    }
 }
