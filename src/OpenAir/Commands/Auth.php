72d9e4342b8995cb795ea2eabb78a8b485dafed0
Curtis Holland <curtis.holland@beaconfire.com>
2016-12-03 12:20:11 -0500

    auth error codes

--- src/OpenAir/Commands/Auth.php
+++ src/OpenAir/Commands/Auth.php
@@ -7,6 +7,30 @@ use OpenAir\Base\Command;
 class Auth extends Command
 {
     private $login_object;
+    public static $response = [
+        "401" => "Auth failed : No such company/user/pass",
+        "402" => "Old TB login Internal TB error",
+        "403" => "No Company name supplied",
+        "404" => "No User name supplied",
+        "405" => "No User Password supplied",
+        "406" => "Invalid Company name",
+        "408" => "Bad Password",
+        "409" => "Account Canceled ",
+        "410" => "Inactive user ",
+        "411" => "Account conflict, contact customer service",
+        "412" => "Wrong namespace for account ",
+        "413" => "Account not privileged to access API",
+        "414" => "Temporarily unavailable",
+        "415" => "Account archived",
+        "416" => "User locked",
+        "417" => "Restricted IP address",
+        "418" => "Invalid uid session ",
+        "419" => "Authentication failed, please retry ",
+        "420" => "Authentication failed ",
+        "421" => "Account misconfiguration or invalid assertion",
+        "422" => "LDAP server unavailable ",
+        "423" => "No permissions to read ServerStatus data"
+    ];
 
     function __construct(\OpenAir\DataTypes\Login $loginObject = null)
     {
